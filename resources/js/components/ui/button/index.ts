5180fd9ef26ca384ae872ff60b4a133c4dc7fd07
drewskiiii956 <vicunapat@gmail.com>
2026-01-13 16:44:53 +0800

    Update dependencies and add migration files for companies, gates, routes, route_stops, vehicle_types, and vehicles

--- resources/js/components/ui/button/index.ts
+++ resources/js/components/ui/button/index.ts
@@ -19,6 +19,8 @@ export const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        create: "bg-dk-grey text-white hover:bg-green-700 focus-visible:ring-green-300 dark:focus-visible:ring-green-400 dark:bg-green-600/80",
+
       },
       size: {
         "default": "h-9 px-4 py-2 has-[>svg]:px-3",
